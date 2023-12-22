0f7a293e707fcc21fb6ccbf30f850140e471a9a6
Adolfo Casas <acasasr@hotmail.com>
2023-12-21 22:32:21 -0600

    rename form1 to frmimport

--- P519-Import/Program.cs
+++ P519-Import/Program.cs
@@ -13,7 +13,7 @@ namespace P519_Import {
       static void Main() {
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
-         Application.Run(new Form1());
+         Application.Run(new FrmImport());
       }
    }
 }
