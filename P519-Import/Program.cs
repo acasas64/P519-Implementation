e2ccda7a1ac7ec2c874d2a955642f7992fc5b8e2
Adolfo Casas <acasasr@hotmail.com>
2023-12-25 13:58:17 -0600

    Changing to Net 6.0

--- P519-Import/Program.cs
+++ P519-Import/Program.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace P519_Import {
    internal static class Program {
       /// <summary>
-      /// Punto de entrada principal para la aplicación.
+      ///  The main entry point for the application.
       /// </summary>
       [STAThread]
       static void Main() {
-         Application.EnableVisualStyles();
-         Application.SetCompatibleTextRenderingDefault(false);
-         Application.Run(new FrmImport());
+         // To customize application configuration such as set high DPI settings or default font,
+         // see https://aka.ms/applicationconfiguration.
+         ApplicationConfiguration.Initialize();
+         Application.Run(new Form1());
       }
    }
 }
