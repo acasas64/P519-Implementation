dbf1c6fcd50c1d5e016a6dbe5558bae0752e1e88
Adolfo Casas <acasasr@hotmail.com>
2024-01-03 13:50:13 -0600

    Menu added to application

--- P519-Import/FrmImport.Designer.cs
+++ P519-Import/FrmImport.Designer.cs
@@ -24,7 +24,14 @@
       /// </summary>
       private void InitializeComponent() {
          dgvImport = new DataGridView();
+         menuStrip1 = new MenuStrip();
+         fileToolStripMenuItem = new ToolStripMenuItem();
+         importMasterSNToolStripMenuItem = new ToolStripMenuItem();
+         importToteSNToolStripMenuItem = new ToolStripMenuItem();
+         toolStripSeparator1 = new ToolStripSeparator();
+         closeToolStripMenuItem = new ToolStripMenuItem();
          ((System.ComponentModel.ISupportInitialize)dgvImport).BeginInit();
+         menuStrip1.SuspendLayout();
          SuspendLayout();
          // 
          // dgvImport
@@ -34,11 +41,54 @@
          dgvImport.AllowUserToOrderColumns = true;
          dgvImport.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
          dgvImport.Dock = DockStyle.Fill;
-         dgvImport.Location = new Point(0, 0);
+         dgvImport.Location = new Point(0, 29);
          dgvImport.Name = "dgvImport";
          dgvImport.ReadOnly = true;
-         dgvImport.Size = new Size(872, 529);
+         dgvImport.Size = new Size(872, 500);
          dgvImport.TabIndex = 0;
+         // 
+         // menuStrip1
+         // 
+         menuStrip1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+         menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+         menuStrip1.Location = new Point(0, 0);
+         menuStrip1.Name = "menuStrip1";
+         menuStrip1.Size = new Size(872, 29);
+         menuStrip1.TabIndex = 1;
+         menuStrip1.Text = "menuStrip1";
+         // 
+         // fileToolStripMenuItem
+         // 
+         fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { importMasterSNToolStripMenuItem, importToteSNToolStripMenuItem, toolStripSeparator1, closeToolStripMenuItem });
+         fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+         fileToolStripMenuItem.Size = new Size(49, 25);
+         fileToolStripMenuItem.Text = "File";
+         // 
+         // importMasterSNToolStripMenuItem
+         // 
+         importMasterSNToolStripMenuItem.Name = "importMasterSNToolStripMenuItem";
+         importMasterSNToolStripMenuItem.Size = new Size(214, 26);
+         importMasterSNToolStripMenuItem.Text = "Import Master SN";
+         importMasterSNToolStripMenuItem.Click += importMasterSNToolStripMenuItem_Click;
+         // 
+         // importToteSNToolStripMenuItem
+         // 
+         importToteSNToolStripMenuItem.Name = "importToteSNToolStripMenuItem";
+         importToteSNToolStripMenuItem.Size = new Size(214, 26);
+         importToteSNToolStripMenuItem.Text = "Import Tote SN";
+         importToteSNToolStripMenuItem.Click += importToteSNToolStripMenuItem_Click;
+         // 
+         // toolStripSeparator1
+         // 
+         toolStripSeparator1.Name = "toolStripSeparator1";
+         toolStripSeparator1.Size = new Size(211, 6);
+         // 
+         // closeToolStripMenuItem
+         // 
+         closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+         closeToolStripMenuItem.Size = new Size(214, 26);
+         closeToolStripMenuItem.Text = "Close";
+         closeToolStripMenuItem.Click += closeToolStripMenuItem_Click;
          // 
          // FrmImport
          // 
@@ -46,15 +96,25 @@
          AutoScaleMode = AutoScaleMode.Font;
          ClientSize = new Size(872, 529);
          Controls.Add(dgvImport);
+         Controls.Add(menuStrip1);
+         MainMenuStrip = menuStrip1;
          Name = "FrmImport";
          Text = "Serials Import From Excel";
-         Load += FrmImport_Load;
          ((System.ComponentModel.ISupportInitialize)dgvImport).EndInit();
+         menuStrip1.ResumeLayout(false);
+         menuStrip1.PerformLayout();
          ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
 
       private DataGridView dgvImport;
+      private MenuStrip menuStrip1;
+      private ToolStripMenuItem fileToolStripMenuItem;
+      private ToolStripMenuItem importMasterSNToolStripMenuItem;
+      private ToolStripMenuItem importToteSNToolStripMenuItem;
+      private ToolStripSeparator toolStripSeparator1;
+      private ToolStripMenuItem closeToolStripMenuItem;
    }
 }
--- P519-Import/FrmImport.cs
+++ P519-Import/FrmImport.cs
@@ -1,24 +1,40 @@
 using ExcelDataReader;
 using System.Data;
+using System.Security.Cryptography.Xml;
 
 namespace P519_Import {
    public partial class FrmImport : Form {
+
+      private readonly string strTitle = "Process Serial Numbers From Excel File";
+
       public FrmImport() {
          InitializeComponent();
       }
 
-      private void FrmImport_Load(object sender, EventArgs e) {
-         string strFileName = AbreArchivo(this);
-         if (strFileName == "") return;
-
-         DataTable dt = LeeArchivo(strFileName);
-         if (dt == null) return; 
-
-         dgvImport.DataSource = dt;
+      private void closeToolStripMenuItem_Click(object sender, EventArgs e) {
+         this.Close();
+         Application.Exit();
       }
 
-      private static string AbreArchivo(IWin32Window owner) {
+      private void importMasterSNToolStripMenuItem_Click(object sender, EventArgs e) {
+         this.Text = strTitle;
+         if (traeArchivo(this, dgvImport, "Process MASTER Serial Number")) {
+            validaMasterSN();
+            this.Text += " (MASTER) ";
+         }
+      }
+
+      private void importToteSNToolStripMenuItem_Click(object sender, EventArgs e) {
+         this.Text = strTitle;
+         if (traeArchivo(this, dgvImport, "Process TOTE Serial Number")) {
+            validaToteSN();
+            this.Text += " (TOTE) ";
+         }
+      }
+
+      private static string pideArchivo(IWin32Window owner, string strWitch) {
          OpenFileDialog ofd = new OpenFileDialog();
+         ofd.Title = strWitch;
          ofd.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
          if (ofd.ShowDialog(owner) == DialogResult.OK) {
             return ofd.FileName;
@@ -26,7 +42,7 @@ namespace P519_Import {
          return "";
       }
 
-      private static DataTable LeeArchivo(string filePath) {
+      private static DataTable leeArchivo(string filePath) {
          System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
          using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read)) {
             using (var reader = ExcelReaderFactory.CreateReader(stream)) {
@@ -42,6 +58,27 @@ namespace P519_Import {
                return table;
             }
          }
+      }
+
+      private static bool traeArchivo(IWin32Window owner, DataGridView dgv, string strWitch) {
+         dgv.DataSource = null;
+         string strFileName = pideArchivo(owner, strWitch);
+         if (strFileName == "") return false;
+
+         DataTable dt = leeArchivo(strFileName);
+         if (dt == null) return false;
+
+         dgv.DataSource = dt;
+
+         return true;
+      }
+
+      private static void validaMasterSN() {
+
+      }
+
+      private static void validaToteSN() {
+
       }
 
    }
